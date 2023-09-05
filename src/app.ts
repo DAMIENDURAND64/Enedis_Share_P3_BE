5a2c9ed4c6438c2bdf18e36f699fff2343ba4c78
DAMIENDURAND64 <damien64durand@gmail.com>
2023-09-05 23:38:11 +0200

    test

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send(
